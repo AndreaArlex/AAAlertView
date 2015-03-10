35451001602d28c70d6a124b52c48ee11f38430f
AndreaArlex <arlexvincy@163.com>
2015-03-10 21:57:00 +0800

    AAAlertView文件添加
    添加了AAAlertView文件，明天可以开始开发，使用autolayout

---
+++ AAAlertViewDemo/AAAlertView/AAAlertView.h
@@ -0,0 +1,13 @@
+//
+//  AAAlertView.h
+//  AAAlertViewDemo
+//
+//  Created by AndreaArlex on 15/3/10.
+//  Copyright (c) 2015年 AndreaArlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAAlertView : UIView
+
+@end
---
+++ AAAlertViewDemo/AAAlertView/AAAlertView.m
@@ -0,0 +1,21 @@
+//
+//  AAAlertView.m
+//  AAAlertViewDemo
+//
+//  Created by AndreaArlex on 15/3/10.
+//  Copyright (c) 2015年 AndreaArlex. All rights reserved.
+//
+
+#import "AAAlertView.h"
+
+@implementation AAAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
